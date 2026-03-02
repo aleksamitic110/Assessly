a2abc95752baf3e4f6c733584a9d7c8e8e3e072e
aleksamitic110 <aleksamitic110@gmail.com>
2026-03-02 16:28:48 +0100

    Sredjen bug

--- server/src/databases/mongodb/routes/statsRoutes.ts
+++ server/src/databases/mongodb/routes/statsRoutes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { getExamStats } from '../controllers/statsController.js';
-import { authenticateToken } from '../../neo4j/middleware/authMiddleware.js';
+import { authenticateJWT } from '../../neo4j/middleware/authMiddleware.js';
 
 const router = Router();
 
-// Zovemo authenticateToken da osiguramo da je korisnik ulogovan
-router.get('/exam/:examId', authenticateToken, getExamStats);
+// Zovemo authenticateJWT da osiguramo da je korisnik ulogovan
+router.get('/exam/:examId', authenticateJWT, getExamStats);
 
 export default router;
